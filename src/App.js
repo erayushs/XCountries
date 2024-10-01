5d86c402c7c078f086b4e36f72e52812e78d950a
Ayush Singh <anmol8960@gmail.com>
2024-10-01 21:28:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://xcountries-backend.azurewebsites.net/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error fetching data: {error.message}</h2>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      {countries.map((country) => (
+        <div className="country-box" key={country.name}>
+          <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
+            className="flag"
+          />
+          <h3>{country.name}</h3>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
